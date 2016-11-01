cc2d78b08fff779dbe42335c2337c11f756e64b7
David Y. Kim <davidykim@gmail.com>
2016-11-01 17:56:05 -0400

    redux setup

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,11 +1,20 @@
 import { combineReducers } from 'redux'
+import allProducts from "./allProducts";
+import allUsers from "./allUsers";
+import cart from "./cart";
+import currentUser from "./currentUser";
+import orderHistory from "./orderHistory";
+import selectedOrder from "./selectedOrder";
+import selectedProduct from "./selectedProduct";
+import selectedUser from "./selectedUser";
 
-const initialState = {}
-
-const rootReducer = function(state = initialState, action) {
-  switch(action.type) {
-    default: return state
-  }
-};
-
-export default rootReducer
+export default combineReducers({
+  allProducts,
+  allUsers,
+  cart,
+  currentUser,
+  orderHistory,
+  selectedOrder,
+  selectedProduct,
+  selectedUser
+});
