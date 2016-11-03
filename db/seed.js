6cb480423a8ae37350cf3b3251ef376e4a949a22
Sia <schen22@gmail.com>
2016-11-03 17:46:35 -0400

    fixed seed file photoUrl

--- db/seed.js
+++ db/seed.js
@@ -7,18 +7,18 @@ const seedUsers = () => db.Promise.map([
 
 const seedProducts = () => db.Promise.map([
 
-  {name: "Grey Goose Vodka", abv: 40, size: "750 ml", price: 40, inventory: 5, photoUrl: "/martini-holder.jpg"},
-  {name: "Barcardi Silver", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/martini-holder.jpg"},
-  {name: "Tanqueray Gin", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/martini-holder.jpg"},
-  {name: "Makers Mark", abv: 40, size: "750 ml", price: 40, inventory: 1, photoUrl: "/martini-holder.jpg"},
-  {name: "Sprite", abv: 40, size: "750 ml", price: 3, inventory: 5, photoUrl: "/martini-holder.jpg"},
-  {name: "Cranberry Juice", abv: 40, size: "750 ml", price: 2, inventory: 3, photoUrl: "/martini-holder.jpg"},
-  {name: "Johnny Walker Black", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/martini-holder.jpg"},
-  {name: "Tonic", abv: 40, size: "750 ml", price: 2, inventory: 2, photoUrl: "/martini-holder.jpg"},
-  {name: "CocaCola", abv: 40, size: "750 ml", price: 3, inventory: 5, photoUrl: "/martini-holder.jpg"},
-  {name: "Stoli Vodka", abv: 40, size: "750 ml", price: 40, inventory: 7, photoUrl: "/martini-holder.jpg"},
-  {name: "Svedka Vodka", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/martini-holder.jpg"},
-  {name: "Jim Beam", abv: 40, size: "750 ml", price: 40, inventory: 5, photoUrl: "/martini-holder.jpg"}
+  {name: "Grey Goose Vodka", abv: 40, size: "750 ml", price: 40, inventory: 5, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Barcardi Silver", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Tanqueray Gin", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Makers Mark", abv: 40, size: "750 ml", price: 40, inventory: 1, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Sprite", abv: 40, size: "750 ml", price: 3, inventory: 5, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Cranberry Juice", abv: 40, size: "750 ml", price: 2, inventory: 3, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Johnny Walker Black", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Tonic", abv: 40, size: "750 ml", price: 2, inventory: 2, photoUrl: "/media/martini-holder.jpg"},
+  {name: "CocaCola", abv: 40, size: "750 ml", price: 3, inventory: 5, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Stoli Vodka", abv: 40, size: "750 ml", price: 40, inventory: 7, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Svedka Vodka", abv: 40, size: "750 ml", price: 30, inventory: 5, photoUrl: "/media/martini-holder.jpg"},
+  {name: "Jim Beam", abv: 40, size: "750 ml", price: 40, inventory: 5, photoUrl: "/media/martini-holder.jpg"}
 
 ], product => db.model('product').create(product))
 
