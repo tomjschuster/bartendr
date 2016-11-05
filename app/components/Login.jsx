d206dabc3beb5fb012b95da75133d698eb1e5d68
Tom Schuster <tomjschuster@gmail.com>
2016-11-05 14:51:17 -0400

    conditional login view

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router';
 
-export const Login = ({ login }) => (
-  <form onSubmit={evt => {
-    evt.preventDefault()
-    login(evt.target.username.value, evt.target.password.value)
-  } }>
-    <input name="username" />
-    <input name="password" type="password" />
-    <input type="submit" value="Login" />
-  </form>
-)
+export const Login = ({ login, auth }) => {return auth ?
+  (
+    <div className="row">
+        <div className="col s3">
+        </div>
+        <div className="col s6">
+          <table className="table">
+              <tr>
+                <td><Link to="/products"><button style={{width: '100%', maxWidth: '250px'}} className="btn">Keep Shopping</button></Link></td>
+                <td><Link to="/cart"><button style={{width: '100%', maxWidth: '250px'}} className="btn">Go to Cart</button></Link></td>
+              </tr>
+          </table>
+        </div>
+        <div className="col s3">
+        </div>
+      </div>
+  ) : (
+    <form onSubmit={evt => {
+      evt.preventDefault()
+      login(evt.target.username.value, evt.target.password.value)
+    } }>
+      <input name="username" placeholder="Email"/>
+      <input name="password" placeholder="Password" type="password" />
+      <input type="submit" value="Login" />
+    </form>)
+}
 
 import {login} from 'APP/app/reducers/auth'
 import {connect} from 'react-redux'
 
 export default connect (
-  state => ({}),
+  ({auth}) => ({auth}),
   dispatch => ({
     login(username, password) {
       return dispatch(login(username, password))
